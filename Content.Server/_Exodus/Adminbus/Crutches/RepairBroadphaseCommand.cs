992db3be8e27463995d3798630db5e5002ad3446
Maxim Bychkov <60302610+Lokilife@users.noreply.github.com>
2026-04-10 00:48:03 +0300

    Repair broadphase command (#192)

---
+++ Content.Server/_Exodus/Adminbus/Crutches/RepairBroadphaseCommand.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using Content.Server.Administration;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server._Exodus.Adminbus.Crutches;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class RepairBroadphaseCommand : IConsoleCommand
+{
+    [Dependency] private readonly IEntityManager _entManager = default!;
+
+    public string Command => "repairbroadphase";
+    public string Description => "Repairs broadphase of specified entity if it was broken.";
+    public string Help => "Usage: repairbroadphase <EntityUid>";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length != 1)
+        {
+            shell.WriteError(Loc.GetString("shell-wrong-arguments-number"));
+            return;
+        }
+
+        if (!NetEntity.TryParse(args[0], out var netEntity))
+        {
+            shell.WriteError(Loc.GetString("shell-entity-uid-must-be-number"));
+            return;
+        }
+
+        var uid = _entManager.GetEntity(netEntity);
+        var lookupSys = _entManager.System<EntityLookupSystem>();
+
+        var xform = _entManager.GetComponent<TransformComponent>(uid);
+        var fieldInfo = typeof(TransformComponent).GetField("Broadphase", BindingFlags.Instance | BindingFlags.NonPublic)!;
+        fieldInfo.SetValue(xform, null);
+        lookupSys.FindAndAddToEntityTree(uid, true, xform);
+    }
+}
